c0f420d43100b7b1d8bd6552f614bb3aa667d018
xavier-wildy <79477645+xavier-wildy@users.noreply.github.com>
2023-10-18 15:29:45 -0400

    Update Users.java

--- Users.java
+++ Users.java
@@ -1,30 +1,21 @@
 package edu.famu.hotel_booking.model;
 
-import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.annotation.DocumentId;
 
-import com.google.protobuf.util.Timestamps;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.text.ParseException;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Users {
     @DocumentId
-    private String passengerID;
-    private String firstName;
-    private String lastName;
-    private Timestamp dateOfBirth; //use google cloud timestamp
+    private String userID;
+    private String name;
     private String email;
     private String phone;
     private String address;
-    private String rewardsNumber;
+    private ;
 
-    public void setDateOfBirth(String dateOfBirth) throws ParseException {
-        this.dateOfBirth = Timestamp.fromProto(Timestamps.parse(dateOfBirth));
-    }
 }
